b108b6f1c0bd92796cc6afefb29d57f7c0a608be
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2024-08-23 17:01:15 -0300

    deploy

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import {useState} from 'react'
+import './App.css'
+import axios from 'axios'
+
+type GithubData = {
+  name: string
+  bio: string
+  avatar_url: string
+}
+
+function App() {
+  const [username, setUsername] = useState("")
+  const [name, setName] = useState("Loading...")
+  const [bio, setBio] = useState("Loading...")
+  const [avatar_url, setAvatarURL] = useState("Loading...")
+ 
+
+
+
+const handlePesquisar = () => {
+  axios.get<GithubData>(`https://api.github.com/users/${username}`).then
+  ((response) => {
+    setName(response.data.name)
+    setBio(response.data.bio)
+    setAvatarURL(response.data.avatar_url)
+})
+
+}
+ 
+
+  return (
+   <div className="container-geral">
+    <div className="container">
+      <header className="header-top">
+        Projeto EMIDES3AM
+      </header>
+      <main>
+        <div className="form">
+          <h1>Pesquisar Github</h1>
+          <input type="text" placeholder="Digite o usuário" onChange={(e) => setUsername(e.target.value)} />
+          <button onClick={handlePesquisar}>Consultar</button>
+          </div>
+          <div className="conteudo">
+            <div>
+            <img src={avatar_url} alt="" />
+            <h1>{name}</h1> 
+            <p>{bio}</p>
+            </div>
+        </div>
+      </main>
+    </div>
+   </div>
+  )
+}
+
+export default App
